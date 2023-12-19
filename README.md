1ce8e4fc0283b6e9b3c068885c1fcd1aa4ae41f2
Victor Navarro <victor.matias.navarro@gmail.com>
2023-12-19 15:52:37 -0800

    Configure MacOS and start to come back :)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ppc
 Solutions for online judges I've made throughout time
+
+# Compiling
+On Linux:
+```
+$ gcc -o prog -std=c++11 -O2 -Wall codigo.cpp
+```
---
+++ beecrowd/3302.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, tmp;
+    cin >> n;
+
+    for(int i=1; i<=n; i++) {
+        cin >> tmp;
+        printf("resposta %d: %d\n", i, tmp);
+    }
+
+    return 0;
+}
