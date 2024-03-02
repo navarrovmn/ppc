00258707e2428bb456088918c4a2b5b84cc8b01b
Victor Navarro <victor.matias.navarro@gmail.com>
2024-03-01 20:22:01 -0800

    Add two numbers

---
+++ leetcode/add_two_numbers.go
@@ -0,0 +1,56 @@
+
+import (
+	"fmt"
+)
+
+ func safeGet(l *ListNode) int {
+     if l == nil {
+         return 0
+     }
+
+     return l.Val
+ }
+
+ func safeGetNext(l *ListNode) *ListNode {
+     if l == nil {
+         return nil
+     }
+
+     return l.Next
+ }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    var tempL1, tempL2 = l1, l2
+    var result = &ListNode{}
+    tmpNode := result
+    var carryOn = 0
+    
+    for {
+        currentSum := safeGet(tempL1) + safeGet(tempL2) + carryOn
+        carryOn = 0
+        currentDigit := currentSum % 10
+        tmpNode.Val = currentDigit
+        
+        if currentSum >= 10 {
+            carryOn = 1
+        }
+
+        tempL1 = safeGetNext(tempL1)
+        tempL2 = safeGetNext(tempL2)
+        if tempL1 == nil && tempL2 == nil && carryOn == 0 {
+            break
+        }
+
+        tmpNode.Next = &ListNode{}
+        tmpNode = tmpNode.Next
+    }
+
+    return result
+}
